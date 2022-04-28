08185bb4f9a24647475b5198b983fe5e92c52633
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-04-28 12:33:13 -0600

    added rollbar

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c967e3ae440143259d8f3f627f404a0e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
